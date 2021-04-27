59876819d040cd1570d98a8effad549b97171903
Ravi Kudumu <ravikudumu@gmail.com>
2021-04-27 19:52:52 +0530

    Update test.battery_charge_ranges.py

--- test.battery_charge_ranges.py
+++ test.battery_charge_ranges.py
@@ -1,9 +1,14 @@
 import unittest
+import battery_charge_ranges
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class test_battery_charge_ranges(unittest.TestCase):
+    def test_charge_ranges_no(self):
+        ranges = battery_charge_ranges.charge_ranges([1,2,3])
+        self.assertEqual(len(ranges), 1)
+    def test_charge_ranges_to_value(self):
+        ranges = battery_charge_ranges.charge_ranges([1, 2, 3, 7])
+        self.assertEqual(ranges[0][-1], 3)
 
 
 if __name__ == '__main__':
